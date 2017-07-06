08d952b5e1536ac0720b3166ae1ac41ed82576a7
youngeun <0eunee0@gmail.com>
2017-07-06 16:29:29 +0900

    mod

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ var chat = io.on('connection', function(socket) {
     });
 });
 
-server.listen(3000, function () {
-    console.log('Socket IO server listening on port 3000');
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+
+// server.listen(3000, function () {
+//     console.log('Socket IO server listening on port 3000');
+// });
