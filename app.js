09dba325cd98c051e193db1cb6faeee2e4644bc4
youngeun <0eunee0@gmail.com>
2017-07-10 14:08:39 +0900

    접속, 이름 수정, 현재 사용자 보기 추가 / 전송버튼 없앰^^ 엔터로!

--- app.js
+++ app.js
@@ -1,27 +1,56 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var redis = require('redis')
+    , mongoose = require('mongoose')
+    , path = require('path');
+    //, swal = require('sweetalert')
 
+app.use(express.static('public'));
 app.get('/', function (req, res) {
-   res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
-var chat = io.on('connection', function(socket) {
+// 현재 접속한 사용자명 저장할 변수
+var usernames = {};
+
+io.sockets.on('connection', function(socket) {
+    socket.on('current user', function () {
+        io.sockets.emit('current user count', usernames);
+    });
+
     socket.on('chat message', function(data){
         console.log('message from client: ', data);
-
-        socket.name = data.name;
-        socket.msg = data.msg;
-        socket.sendTime = data.sendTime;
-
-        io.emit('chat message', data);
+        io.sockets.emit('chat message', data);
     });
+
+    socket.on('add user', function (username) {
+        console.log(username + ' 접속');
+        // 소켓 세션의 username 필드에 사용자가 전송한 값 저장
+        socket.username = username;
+        usernames[username] = username;
+        socket.emit('connect chat', 'SERVER', 'you have connected');
+        socket.broadcast.emit('connect chat', 'SERVER', username + ' has connected');
+        io.sockets.emit('connect users', usernames);
+    });
+
+    socket.on('mod user', function (username, nusername) {
+       console.log(username + '이 ' + nusername + ' 로 닉네임 변경');
+       delete usernames[username];
+       socket.username = nusername;
+       usernames[nusername] = nusername;
+       io.sockets.emit('connect users', usernames);
+    });
+
+    socket.on('disconnect', function () {
+        console.log(socket.username + ' 접속 해제');
+        delete usernames[socket.username];
+        io.sockets.emit('connect users', usernames);
+        socket.broadcast.emit('connect chat', 'SERVER', socket.username + ' has disconnected');
+    })
 });
 
 server.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
-
-// server.listen(3000, function () {
-//     console.log('Socket IO server listening on port 3000');
-// });
